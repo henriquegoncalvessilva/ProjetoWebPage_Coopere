c1ac617aa9e85a661f6b32165d4d667381c75324
Henrique Gonçalves <luizhenriquegoncalvessilva@hotmail.com>
2022-01-20 17:03:57 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,32 @@ const body = document.querySelector('body');
 mobileMenu.style.display = 'none';
 const btnMobileClose = document.querySelector('#btn-close');
 
+const progressBarListValues = document.querySelectorAll('.progress-porcent');
+
+const progressBarList = document.querySelectorAll('.progress');
+
+const progressPorcent = document.querySelectorAll('.progress-porcent');
+
+console.log(progressPorcent);
+
+
+for(var i = 0; i < progressBarListValues.length; i++){
+
+
+    progressBarList[i].style.width = progressBarListValues[i].innerHTML;
+    
+    const result = progressBarList[i].style.width = progressBarListValues[i].innerHTML;
+
+    // progressPorcent[i].style.margin = "1rem " + progressBarList[i].style.width;
+
+
+    
+}
+
+
+
+
+
 
 function closeMenuMobile(){
     mobileMenu.style.display ="none";
